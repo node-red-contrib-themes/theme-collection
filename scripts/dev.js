4a5f837b943c48422753e3cedcdd61a5bce9d5cc
Mauricio Bonani <bonanitech@gmail.com>
2022-09-17 15:43:42 -0400

    Re-work dev script

--- scripts/dev.js
+++ scripts/dev.js
@@ -4,35 +4,38 @@ const options = commandLineArgs([{ name: 'themeName', type: String, defaultOptio
 const path = require('path')
 const { existsSync } = require('fs')
 const nodemon = require('nodemon')
+const themeName = String(options.themeName.split('-scroll', 1))
+const themePath = options.themeName ? path.join('themes', themeName) : ''
 
 if (!options.themeName) {
-    nodemon({
-        exec: `node-red/packages/node_modules/node-red/red.js --port 41880 --userDir .node-red`,
-        ext: 'js,json,css',
-        watch: [
-            "common/",
-            "themes/"
-        ],
-    })
-} else {
-    const themeDir = path.join('themes', String(options.themeName.split("-scroll", 1)))
-    if (!existsSync(themeDir)) {
-        console.warn('')
-        console.warn(`Theme path is not valid. Could not find '${themeDir}'`)
-        console.warn('Please create and build the theme first')
-        console.warn('')
-        console.warn('Example:')
-        console.warn(`npm run create-theme ${options.themeName}`)
-        console.warn(`npm run build-theme ${options.themeName}`)
-        process.exit(2)
-    }
+    showUsageAndExit(1)
+}
 
-    nodemon({
-        exec: `node-red/packages/node_modules/node-red/red.js --port 41880 --userDir .node-red --define editorTheme.theme=${options.themeName}`,
-        ext: 'js,json,css',
-        watch: [
-            "common/",
-            "themes/"
-        ],
-    })
+if (options.themeName && !existsSync(themePath)) {
+    console.warn('')
+    console.warn(`Theme path is not valid. Could not find '${themePath}'`)
+    console.warn('Please create and build the theme first')
+    console.warn('')
+    console.warn('Example:')
+    console.warn(`npm run create-theme ${themeName}`)
+    console.warn(`npm run build-theme ${themeName}`)
+    process.exit(2)
+}
+
+nodemon({
+    exec: `node-red/packages/node_modules/node-red/red.js --port 41880 --userDir .node-red --define credentialSecret=false --define editorTheme.projects.enabled=true --define editorTheme.theme=${options.themeName} theme-dev-project`,
+    ext: 'js,json,css',
+    watch: [
+        'common/',
+        'themes/'
+    ],
+    restartable: false
+})
+
+function showUsageAndExit(exitCode) {
+    console.log('')
+    console.log('Usage: dev theme-name')
+    console.log('Example: npm run dev theme-name')
+    console.log('Example: node ./scripts/dev.js theme-name')
+    process.exit(exitCode)
 }
