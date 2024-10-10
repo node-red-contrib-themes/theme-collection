c8bcaea326b916576222fbe0f8f4c474c41e796c
Mauricio Bonani <bonanitech@gmail.com>
2024-10-10 17:06:32 -0400

    Add Prettier (#528)

--- themes.js
+++ themes.js
@@ -9,17 +9,17 @@ module.exports = function (RED) {
     for (const themeName of themes) {
         const themePath = `themes/${themeName}`
 
-        RED.plugins.registerPlugin(
-            themeName,
-            {
-                type: 'node-red-theme',
-                css: [`${themePath}/${themeName}.min.css`],
-                mermaid: { theme: require(`./${themePath}/${themeName}-mermaid.min.json`) },
-                monacoOptions: { theme: require(`./${themePath}/${themeName}-monaco.min.json`) }
+        RED.plugins.registerPlugin(themeName, {
+            type: 'node-red-theme',
+            css: [`${themePath}/${themeName}.min.css`],
+            mermaid: {
+                theme: require(`./${themePath}/${themeName}-mermaid.min.json`)
+            },
+            monacoOptions: {
+                theme: require(`./${themePath}/${themeName}-monaco.min.json`)
             }
-        )
+        })
     }
 
     RED.log.info(`Node-RED Contrib Theme Collection version: v${package.version}`)
-
 }
